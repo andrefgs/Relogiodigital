309edbd92a1cd27bd42d3940946501a0c3fe7c8f
andrefgs <adreafgs@gmail.com>
2025-02-20 09:52:04 -0300

    Atualizacao PM/AM

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,7 +1,6 @@
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
-const amoupm = document.getElementById('ampm');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -19,16 +18,5 @@ const relogio = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
-
-    let ampm = horas >= 12 ? 'PM' : 'AM';
-    horas = horas % 12;
-    horas = horas ? horas : 12; 
-    minutos = minutos < 10 ? '0' + minutos : minutos;
-    segundos = segundos < 10 ? '0' + segundos : segundos;
-    
-    horas.textContent = horas < 10 ? '0' + horas : horas;
-    minutos.textContent = minutos;
-    segundos.textContent = segundos;
-    amoupm.textContent = ampm;
 
 })
